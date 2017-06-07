56c9187edf342515fe98f1bb3b3a636949535617
mengalong <alongmeng@gmail.com>
2017-06-07 20:09:14 +0800

    add the parallel scripts

---
+++ shell/parallel/README.md
@@ -0,0 +1,13 @@
+# Shell脚本实现并发
+
+# 功能：
+
+在linux下，使用shell脚本实现并发执行任务.
+
+# 使用方法:
+
+以 $batch\_num 的并发度执行$cmd 命令 $max\_times 次
+
+bash para\_do.sh $batch\_num $max\_times "$cmd"
+
+
---
+++ shell/parallel/para_do.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+tempfifo=$$.fifo
+
+if [ $# -eq 3 ] 
+then
+	batch_num=$1
+	max_times=$2
+	cmd=$3
+else
+    echo "Error! Not enough params."
+    echo "Sample: bash para_do.sh 10 100 '\$cmd'"
+    exit 2;
+fi
+
+
+trap "exec 1000>&-;exec 1000<&-;exit 0" 2
+mkfifo $tempfifo
+exec 1000<>$tempfifo
+rm -rf $tempfifo
+
+for ((i=1; i<=$batch_num; i++))
+do
+    echo >&1000
+done
+
+start_time=0
+while [ $start_time -lt $max_times ]
+do
+	echo "$start_time start..."
+    read -u1000
+    {
+		$cmd
+        echo >&1000
+    } &
+	echo "$start_time end..."
+	((start_time++))
+done
+
+wait
+echo "all done!!"
