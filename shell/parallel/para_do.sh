2e7324d56e76cc10cae66829dbe4d5732de93eba
mengalong <alongmeng@gmail.com>
2017-06-07 20:12:16 +0800

    modify para_do.sh

--- shell/parallel/para_do.sh
+++ shell/parallel/para_do.sh
@@ -27,11 +27,11 @@ start_time=0
 while [ $start_time -lt $max_times ]
 do
 	echo "$start_time start..."
-    read -u1000
-    {
+	read -u1000
+	{
 		$cmd
 		echo >&1000
-    } &
+	} &
 	echo "$start_time end..."
 	((start_time++))
 done
