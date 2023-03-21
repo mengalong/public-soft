dbb0cd2832f530c55d1be584de56106d63812572
mengalong <mengalong>
2023-03-21 17:29:46 +0800

    add json example
    rt,add json/yaml example

---
+++ python/json-yaml/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import yaml
+import sys
+
+def load_yaml(filename):
+    data = None
+    with open(filename, "r", encoding="utf-8") as fp:
+        data = yaml.load(fp, Loader=yaml.CLoader)
+    return data
+
+def write_yaml(filename, data):
+    with open(filename, "w", encoding="utf-8") as fp:
+        yaml.dump(data, fp, allow_unicode=True)
+
+def load_json(filename):
+    data = None
+    with open(filename, "r") as fp:
+        data = json.load(fp)
+    return data
+
+def json_print(data):
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(',',":")))
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
